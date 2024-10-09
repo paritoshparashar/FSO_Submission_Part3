206ef7029e69ff3d98948c64ead26af284a3ab47
paritoshparashar <papa00001@stud.uni-saarland.de>
2024-10-09 14:50:34 +0200

    Server port update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
-const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
+
+const app = express()
 
 let phoneBookEntries = [
     { 
@@ -34,6 +36,7 @@ morgan.token('postData' , (req) => {
   return '';
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'))
 
@@ -98,14 +101,16 @@ app.post ('/api/persons', (req, res) => {
 
 app.delete('/api/persons/:id', (req, res) => {
 
-  const id = Number(req.params.id)
+  const id = req.params.id
   phoneBookEntries = phoneBookEntries.filter((entry) => entry.id !== id)
 
   res.status(204).end()
   
 })
 
-const PORT = 3001
+
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}`)
 })
